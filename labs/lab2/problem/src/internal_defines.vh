bde1511b094d92c6f878fda01ddde82d725cc16a
younghojan <younghojann@gmail.com>
2023-06-07 19:25:58 +0800

    Complete the part of arithmetic operation in CU

--- labs/lab2/problem/src/internal_defines.vh
+++ labs/lab2/problem/src/internal_defines.vh
@@ -2,6 +2,33 @@
 //// Internal signal constants
 ////
 
-// ALU
-`define ALU_ADD      4'b0000
-`define ALU_SUB      4'b0001
+
+// ALUOp
+// arithmetic operation
+`define ALUOp_ADD       4'b0000
+`define ALUOp_SUB       4'b0001
+
+// bit operation
+`define ALUOp_AND       4'b0010
+`define ALUOp_OR        4'b0011
+`define ALUOp_XOR       4'b0100
+`define ALUOp_NOR       4'b0101
+
+// shift operation
+`define ALUOp_SHL       4'b0110
+`define ALUOp_SHLV      4'b0111
+`define ALUOp_SHRL      4'b1000
+`define ALUOp_SHRLV     4'b1001
+`define ALUOp_SHRA      4'b1010
+`define ALUOp_SHRAV     4'b1011
+
+
+// ALUSrc
+// ALUSrc1
+`define ALUSrc1_REG     1'b0    // rs
+`define ALUSrc1_SHAMT   1'b1    // Shift Amount
+
+// ALUSrc2
+`define ALUSrc2_REG     2'b00   // rt
+`define ALUSrc2_ZEIMM   2'b01   // Zero-Extended
+`define ALUSrc2_SEIMM   2'b10   // Sign-Extended
--- labs/lab2/problem/src/mips_CU.sv
+++ labs/lab2/problem/src/mips_CU.sv
@@ -1,41 +1,3 @@
-/*
- *
- * Redistributions of any form whatsoever must retain and/or include the
- * following acknowledgment, notices and disclaimer:
- *
- * This product includes software developed by Carnegie Mellon University. 
- *
- * Copyright (c) 2004 by Babak Falsafi and James Hoe,
- * Computer Architecture Lab at Carnegie Mellon (CALCM), 
- * Carnegie Mellon University.
- *
- * This source file was written and maintained by Jared Smolens 
- * as part of the Two-Way In-Order Superscalar project for Carnegie Mellon's 
- * Introduction to Computer Architecture course, 18-447. The source file
- * is in part derived from code originally written by Herman Schmit and 
- * Diana Marculescu.
- *
- * You may not use the name "Carnegie Mellon University" or derivations 
- * thereof to endorse or promote products derived from this software.
- *
- * If you modify the software you must place a notice on or within any 
- * modified version provided or made available to any third party stating 
- * that you have modified the software.  The notice shall include at least 
- * your name, address, phone number, email address and the date and purpose 
- * of the modification.
- *
- * THE SOFTWARE IS PROVIDED "AS-IS" WITHOUT ANY WARRANTY OF ANY KIND, EITHER 
- * EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO ANYWARRANTY 
- * THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS OR BE ERROR-FREE AND ANY 
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, 
- * TITLE, OR NON-INFRINGEMENT.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY 
- * BE LIABLE FOR ANY DAMAGES, INCLUDING BUT NOT LIMITED TO DIRECT, INDIRECT, 
- * SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR IN 
- * ANY WAY CONNECTED WITH THIS SOFTWARE (WHETHER OR NOT BASED UPON WARRANTY, 
- * CONTRACT, TORT OR OTHERWISE).
- *
- */
-
 // Include the MIPS constants
 `include "mips_defines.vh"
 `include "internal_defines.vh"
@@ -44,31 +6,448 @@
 //// CU: Control Unit
 ////
 //// op_code  (input)  - Instruction opcode
-//// RegDst   (output) - 
-//// Jump     (output) - 
-//// Branch   (output) - 
-//// MemRead  (output) - 
-//// MemtoReg (output) - 
-//// ALUop    (output) - 
-//// MemWrite (output) - 
-//// ALUSrc   (output) - 
-//// RegWrite (output) - 
+//// rt       (input)  - BGEZ or BGEZAL or BLTZ or BLTZAL 
+//// funct    (input)  - Function field
 ////
-module CU(/*AUTOARG*/
-   // Outputs
-   RegDst, Jump, Branch, MemRead, MemtoReg, ALUop, MemWrite, ALUSrc, RegWrite,
+//// RegDst   (output) - Register destination control signal
+//// RegWrite (output) - Register write control signal
+//// Jump     (output) - Jump control signal
+//// Branch   (output) - Branch control signal
+//// MemRead  (output) - Memory read control signal
+//// MemWrite (output) - Memory write control signal
+//// MemtoReg (output) - Memory to register data control signal
+//// ALUop    (output) - ALU operation control signal
+//// ALUSrc1  (output) - ALU operand 1 control signal
+//// ALUSrc2  (output) - ALU operand 2 control signal
+////
+module CU(
    // Inputs
-   op_code
-   );
+   op_code,
+   rt,
+   funct,
+   // Outputs
+   RegDst,
+   RegWrite
+   Jump,
+   Branch,
+   MemRead,
+   MemWrite,
+   MemtoReg,
+   ALUOp,
+   ALUSrc1,
+   ALUSrc2
+);
 
    input [5:0] op_code;
-   output reg  RegDst,jump,Branch,MemtoReg,ALUSrc,RegWrite,MemWrite,MemRead;
-   output reg  [3:0] ALUop;
+   input [5:0] funct;
+   output reg  RegDst, RegWrite, Jump, Branch, MemRead, MemWrite, MemtoReg, ALUSrc1;
+   output reg  [1:0] ALUSrc2;
+   output reg  [3:0] ALUOp;
 
    always @(*) begin
+      case(op_code)
 
-     case(op_code)
+         `OP_OTHER0:  // SPECIAL
+            begin
+               case(funct)
+                  
+                  `OP0_ADD:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_ADD;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
 
+                  `OP0_ADDU:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_ADD;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_AND:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_AND;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_JALR:
+                     begin
+
+                     end
+
+                  `OP0_JR:
+                     begin
+
+                     end
+
+                  `OP0_NOR:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_NOR;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_OR:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_OR;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SLL:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SHL;
+                        ALUSrc1 = `ALUSrc1_SHAMT;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SLLV:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SHLV;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SLT:
+                     begin
+                        
+                     end
+
+                  `OP0_SRA:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SHRA;
+                        ALUSrc1 = `ALUSrc1_SHAMT;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SRAV:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SHRAV;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SRL:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SHRL;
+                        ALUSrc1 = `ALUSrc1_SHAMT;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SRLV:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SHRLV;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SUB:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SUB;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SUBU:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_SUB;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  `OP0_SYSCALL:
+                     begin
+                        
+                     end
+
+                  `OP0_XOR:
+                     begin
+                        RegDst = 1;
+                        RegWrite = 1;
+                        Jump = 0;
+                        Branch = 0;
+                        MemRead = 0;
+                        MemWrite = 0;
+                        MemtoReg = 0;
+                        ALUOp = `ALUOp_XOR;
+                        ALUSrc1 = `ALUSrc1_REG;
+                        ALUSrc2 = `ALUSrc2_REG;
+                     end
+
+                  default:
+                     begin
+                        
+                     end
+              endcase
+
+           end
+
+         `OP_ADDI:
+            begin
+               RegDst = 0;
+               RegWrite = 1;
+               Jump = 0;
+               Branch = 0;
+               MemRead = 0;
+               MemWrite = 0;
+               MemtoReg = 0;
+               ALUOp = `ALUOp_ADD;
+               ALUSrc1 = `ALUSrc1_REG
+               ALUSrc2 = `ALUSrc2_SEIMM;
+            end
+
+         `OP_ADDIU:
+            begin
+               RegDst = 0;
+               RegWrite = 1;
+               Jump = 0;
+               Branch = 0;
+               MemRead = 0;
+               MemWrite = 0;
+               MemtoReg = 0;
+               ALUOp = `ALUOp_ADD;
+               ALUSrc1 = `ALUSrc1_REG
+               ALUSrc2 = `ALUSrc2_SEIMM;
+            end
+
+         `OP_ANDI:
+            begin
+               RegDst = 0;
+               RegWrite = 1;
+               Jump = 0;
+               Branch = 0;
+               MemRead = 0;
+               MemWrite = 0;
+               MemtoReg = 0;
+               ALUOp = `ALUOp_AND
+               ALUSrc1 = `ALUSrc1_REG;
+               ALUSrc2 = `ALUSrc2_ZEIMM;
+            end
+
+         `OP_BEQ:
+            begin
+
+            end
+
+         `OP_OTHER1: // REGIMM
+            begin
+               case(rt)
+
+                  `OP1_BGEZ:
+                     begin
+                        
+                     end
+
+                  `OP1_BGEZAL
+                     begin
+                        
+                     end
+
+                  `OP1_BLTZ:
+                     begin
+                        
+                     end
+
+                  `OP1_BLTZAL:
+                     begin
+                        
+                     end
+               endcase
+            end
+
+         `OP_BGTZ:
+            begin
+               
+            end
+
+         `OP_BLEZ:
+            begin
+               
+            end
+
+         `OP_BNE:
+            begin
+               
+            end
+
+         `OP_J:
+            begin
+
+            end
+
+         `OP_JAL:
+            begin
+               
+            end
+
+         `OP_LBU:
+            begin
+               
+            end
+
+         `OP_LH:
+            begin
+               
+            end
+
+         `OP_LHU:
+            begin
+               
+            end
+
+         `OP_LUI:
+            begin
+               
+            end
+
+         `OP_LW:
+            begin
+
+            end
+
+         `OP_ORI:
+            begin
+               RegDst = 0;
+               RegWrite = 1;
+               Jump = 0;
+               Branch = 0;
+               MemRead = 0;
+               MemWrite = 0;
+               MemtoReg = 0;
+               ALUOp = `ALUOp_OR;
+               ALUSrc = `ALUSrc_ZEIMM;
+            end
+
+         `OP_SB:
+            begin
+               
+            end
+
+         `OP_SH:
+            begin
+               
+            end
+
+         `OP_SLTI:
+            begin
+               
+            end
+
+         `OP_SLTIU:
+            begin
+               
+            end
+
+         `OP_SW:
+            begin
+               
+            end
+
+         `OP_XORI:
+            begin
+               RegDst = 0;
+               RegWrite = 1;
+               Jump = 0;
+               Branch = 0;
+               MemRead = 0;
+               MemWrite = 0;
+               MemtoReg = 0;
+               ALUOp = `ALUOp_XOR;
+               ALUSrc = `ALUSrc_ZEIMM;
+            end
      endcase
    end
 
